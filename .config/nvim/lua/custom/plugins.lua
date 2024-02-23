265c41c60089577561bffe1dc3515e93e92e895e
Springfield Yonga <springfieldyonga@outlook.com>
2024-02-23 17:20:00 +0100

    installed poimandres theme on neovim

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -53,6 +53,24 @@ local plugins = {
     opts = overrides.flash.opts,
     -- stylua: ignore
     keys = overrides.flash.keys,
+  },
+
+  {
+    'olivercederborg/poimandres.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('poimandres').setup {
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      }
+    end,
+
+    -- optionally set the colorscheme within lazy config
+    init = function()
+      vim.cmd("colorscheme poimandres")
+    end
   }
   -- To make a plugin not be loaded
   -- {
